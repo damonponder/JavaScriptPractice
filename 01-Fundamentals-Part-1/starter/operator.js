db69f375100c35d99b79746a05570708ab4ddccb
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-10-21 00:20:34 -0500

    Update to Javacript practive for operators

---
+++ 01-Fundamentals-Part-1/starter/operator.js
@@ -0,0 +1,39 @@
+
+//Math Operators
+const currentYear = 2020;
+const ageDamon = 1974;
+const ageSarah = 1991;
+
+const currentAgeDamon = currentYear - ageDamon;
+const currentAgeSarah = currentYear - ageSarah;
+console.log(currentYear, ageDamon);
+
+console.log(currentYear - ageDamon);
+
+console.log(ageDamon * 2, ageDamon / 10, 2**3);
+
+// 2 is to raise something to the power of a number
+
+const firstName = 'Damon';
+const lastName = 'Ponder';
+
+console.log(firstName + ' ' + lastName);
+
+//Assignment Operators
+
+let x = 10 + 5;
+
+x += 10; // x equal x + 10 = 25
+
+x *= 4; // x = x * 4 = 100
+
+x++;
+
+x--;
+
+console.log(x);
+
+//Comparison Operator
+
+console.log(currentAgeDamon > currentAgeSarah);
+console.log(currentAgeSarah >= 18);
--- 01-Fundamentals-Part-1/starter/practice1.js
+++ 01-Fundamentals-Part-1/starter/practice1.js
@@ -1,3 +1,22 @@
-let js = 'amazing';
+// let js = 'amazing';
+// console.log(40+8+23-10);
 
-if (js === 'amazing') alert('Javascript is Fun!')
+// function Client(fn, ln, a, g, e) {
+//     let firstName = "Damon"
+//     let lastName = "Ponder"
+//     const age = 46
+//     const gender = 'Male'
+//     const ethnicity = 'White'
+//     return Client;
+// }
+// console.log(Client)
+
+// function Variables() {
+//     let country = 'United States';
+//     let continent = 'North America';
+//     let population = '300 million';
+// return Variables;
+
+// }
+// console.log(Variables);
+
