03ab72e97f1cfc7dbcfa9b7827b5e176ed70bd63
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-10-27 18:05:09 -0500

    final update to objects

--- 02-Fundamentals-Part-2/starter/objects.js
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -12,19 +12,19 @@ const jonasObject = {
 // Dot vs. Bracket Notation
 // We can get the object by using dot notation
 
-console.log(jonasObject.firstName)
-console.log(jonasObject['lastName']);
+// console.log(jonasObject.firstName)
+// console.log(jonasObject['lastName']);
 
-const nameKey = 'Name';
-console.log(jonasObject['first' + nameKey]);
-console.log(jonasObject['last' + nameKey]);
+// const nameKey = 'Name';
+// console.log(jonasObject['first' + nameKey]);
+// console.log(jonasObject['last' + nameKey]);
 
-const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, friends, or Job');
-console.log(jonasObject[interestedIn]);
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, friends, or Job');
+// console.log(jonasObject[interestedIn]);
 
-if(jonasObject[interestedIn]) {
-    console.log([jonasObject[interestedIn]])
-} else {
-    console.log('Wrong request!')
-}
+// if(jonasObject[interestedIn]) {
+//     console.log([jonasObject[interestedIn]])
+// } else {
+//     console.log('Wrong request!')
+// }
 
