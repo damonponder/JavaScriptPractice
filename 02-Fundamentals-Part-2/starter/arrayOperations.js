5f0bafd9bf9f091456fa39d00ddad8d3a74dbc7c
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-10-27 01:54:07 -0500

    commit before stopping for the night

---
+++ 02-Fundamentals-Part-2/starter/arrayOperations.js
@@ -0,0 +1,13 @@
+//Array Methods
+
+const friends1 = ['michael', 'peter', 'wes'];
+
+//Add elements to end of Array
+friends1.push('Jay');
+console.log(friends1)
+//Add elements to beginning of Array
+friends1.unshift('John')
+console.log(friends1);
+//Remove elements from end of Array
+friends1.pop();
+console.log(friends1)
