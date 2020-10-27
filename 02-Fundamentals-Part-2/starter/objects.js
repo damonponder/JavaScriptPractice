02d5d54557c0ea1f13a8b621c783da919c083ea4
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-10-27 17:46:18 -0500

    Basic Object Structure

---
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -0,0 +1,10 @@
+//This object has 5 properties
+// This is considered object literal syntax
+
+const jonasObject = {
+    firstName: 'Jonas',
+    lastName: 'Schmegman',
+    Job: 'Teacher',
+    age: 2020 - 1991,
+    friends: ['Michael', 'Peter', 'Steven']
+};
