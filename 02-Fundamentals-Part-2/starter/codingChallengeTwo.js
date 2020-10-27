38d8ba8c4eff77eaa2e7fee3d32d610752ce3b8b
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-10-27 18:02:09 -0500

    Updates to objects

--- 02-Fundamentals-Part-2/starter/codingChallengeTwo.js
+++ 02-Fundamentals-Part-2/starter/codingChallengeTwo.js
@@ -1,14 +1,14 @@
 
-billAmount1 = Number(prompt('Enter Your Bill Amount!'))
-const bills = [billAmount1]
+// billAmount1 = Number(prompt('Enter Your Bill Amount!'))
+// const bills = [billAmount1]
 
-const tipCalc = function (bill) {
-    tip1 = bill * .15;
-    tip2 = bill * .2;
-    return bill >= 50 && bill <= 300 ? tip1 : tip2
-}
+// const tipCalc = function (bill) {
+//     tip1 = bill * .15;
+//     tip2 = bill * .2;
+//     return bill >= 50 && bill <= 300 ? tip1 : tip2
+// }
     
-const tipsTotal = [tipCalc(bills[0])];
-const totalBills = [bills[0] +  tipsTotal[0]]
+// const tipsTotal = [tipCalc(bills[0])];
+// const totalBills = [bills[0] +  tipsTotal[0]]
 
-console.log(bills, tipsTotal, totalBills)
+// console.log(bills, tipsTotal, totalBills)
--- 02-Fundamentals-Part-2/starter/objects.js
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -8,3 +8,23 @@ const jonasObject = {
     age: 2020 - 1991,
     friends: ['Michael', 'Peter', 'Steven']
 };
+
+// Dot vs. Bracket Notation
+// We can get the object by using dot notation
+
+console.log(jonasObject.firstName)
+console.log(jonasObject['lastName']);
+
+const nameKey = 'Name';
+console.log(jonasObject['first' + nameKey]);
+console.log(jonasObject['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, friends, or Job');
+console.log(jonasObject[interestedIn]);
+
+if(jonasObject[interestedIn]) {
+    console.log([jonasObject[interestedIn]])
+} else {
+    console.log('Wrong request!')
+}
+
