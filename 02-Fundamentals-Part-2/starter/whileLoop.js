dda126611d009b5b3cb34a271f88a1b0b72475a9
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-10-28 22:20:41 -0500

    final update  to while loops

--- 02-Fundamentals-Part-2/starter/whileLoop.js
+++ 02-Fundamentals-Part-2/starter/whileLoop.js
@@ -8,5 +8,9 @@ while (rep <= 10) {
     rep++
 }
 
+let dice = Math.trunc(Math.random() * 6) + 1;
 
-
+while (dice !== 6) {
+    console.log(`you rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+}
