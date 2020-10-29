f294ea5540f2c4b3c41437ea3bed24ff107bb89a
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-10-28 22:13:05 -0500

    Update to while loops

---
+++ 02-Fundamentals-Part-2/starter/whileLoop.js
@@ -0,0 +1,12 @@
+// for(let rep = 1; rep <= 10; rep++) {
+//         console.log(`Lifting weights repetition ${rep}`)
+//     }
+    
+ let rep = 1   
+while (rep <= 10) {
+    console.log(`WHILE: Lifting weights repitition ${rep}`);
+    rep++
+}
+
+
+
