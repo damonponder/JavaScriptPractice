601ab7b123ba525467cd1b1e4586f771634c0832
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-10-28 21:33:56 -0500

    Update to looping backwards

--- 02-Fundamentals-Part-2/starter/loopinBackwards.js
+++ 02-Fundamentals-Part-2/starter/loopinBackwards.js
@@ -1,11 +1,19 @@
-const damonArray = [
-    'Damon',
-    'Ponder',
-    2020-1974,
-    'teacher',
-    ['Justin', 'Justo', 'Matt']
-];
+// const damonArray = [
+//     'Damon',
+//     'Ponder',
+//     2020-1974,
+//     'teacher',
+//     ['Justin', 'Justo', 'Matt']
+// ];
+// //           (counter),           (condition),  {counter update)
+// for (let i = damonArray.length - 1; i >= 0; i--) {
+//     console.log(i, damonArray[i])
+// }
 
-for (let i = damonArray.length - 1; i >= 0; i--) {
-    console.log(i, damonArray[i])
-}
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`----------Starting Exerrcise ${exercise}`)
+
+    for ( let rep =1; rep < 6; rep++) {
+        console.log(`Lifting Repetition ${rep}`)
+    }
+};
