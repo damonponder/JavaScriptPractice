8fba3daf2125ec7e02ff011a40c00889fbd0b2de
Damon Ponder <43759421+damonponder@users.noreply.github.com>
2020-11-09 16:12:14 -0600

    updates to DOM manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!'
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+
+// document.querySelector('.guess').value =23;
+// console.log(document.querySelector('.guess').value);
+
+
+
+
+
+
+
+
+
+
+
+
+
